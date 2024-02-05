43feaafde7b164cbc0a5f42bfa9e7c856654f671
Splashkie <asterrflash@yandex.ru>
2024-02-05 22:03:39 +0300

    start

---
+++ BlazorApp7/BlazorApp7/Models/DbHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace BlazorApp7.Models
+{
+    public class DbHelper : DbContext
+    {
+        public DbSet<User> users { get; set; } = null!;
+        public string connstring = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=fhntv123";
+        public DbHelper()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connstring);
+        }
+    }
+}
